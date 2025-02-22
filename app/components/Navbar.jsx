01f267cf01008b7624da8c1e166b1eb31dabd44d
Bhupendra Choudhary <80590016+Bhupen15@users.noreply.github.com>
2025-02-22 13:31:58 +0530

    second commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -54,7 +54,7 @@ useEffect(()=>{
 
                 </div>
 
-                      {/* ----------------    Mobile Menu ----------------------------- */}
+                      {/* ---------------- This is Mobile Menu ----------------------------- */}
 
                       <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white'>         
 
