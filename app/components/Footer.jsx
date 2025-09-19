0c34b58fd217e37e070aa1caf1635292a13c4fb4
Bhupendra Choudhary <80590016+Bhupen15@users.noreply.github.com>
2025-09-20 01:52:53 +0530

    Removed gmail from footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = ({isDarkMode}) => {
     <div className='text-center'>
       <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2' />
 
-      <div className='w-max flex items-center gap-2 mx-auto'>
+      {/* <div className='w-max flex items-center gap-2 mx-auto'>
       <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6 ' />
       bhupendrachoudhary1746@gmail.com
-      </div>
+      </div> */}
     </div>
 <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] py-6 mt-12'>
     <p>© 2025 Bhupendra Choudhary. All rights reserved.</p>
